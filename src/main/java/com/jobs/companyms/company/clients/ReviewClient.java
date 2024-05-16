538d16cff70768288ab030b4daee0af043c6e6bb
Rohit Kumar <rohit.kr.0310@gmail.com>
2024-05-16 11:16:05 +0200

    configure app to run on docker

--- src/main/java/com/jobs/companyms/company/clients/ReviewClient.java
+++ src/main/java/com/jobs/companyms/company/clients/ReviewClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEW-SERVICE")
+@FeignClient(name = "REVIEW-SERVICE", url = "${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews/averageRating")
     Double  getAverageRating(@RequestParam("companyId") Long companyId);
